2c9ee1820f0a2ccfd64668d70b0a0e6be80930fc
Mathieu Ranc <dev@mathieuranc.fr>
2021-09-24 15:01:00 +0200

    mid

---
+++ TD3 - PHP MySQL 1/test.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>test</title>
+</head>
+
+<body>
+  <h1>Je suis un fichier de test de fonctionnement du PHP</h1>
+  <?php
+  $i = 0;
+  while ($i < 10) {
+    echo "<h2>Tirage d'un nombre al&eacute;atoire : " . rand(0, 100) . "</h2>";
+    $i = $i + 1;
+  }
+  ?>
+  <p>*********************************************</p>
+  <?php
+  echo "<p align='center'>Today is " . date("l d F Y") . "</p>";
+  ?>
+</body>
+
+</html>
