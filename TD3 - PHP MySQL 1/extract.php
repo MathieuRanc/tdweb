916620b025e9ef3fc2a388680e73d8c4bf3cf547
Mathieu Ranc <dev@mathieuranc.fr>
2021-09-24 16:06:30 +0200

    not connected to db

---
+++ TD3 - PHP MySQL 1/extract.php
@@ -0,0 +1,30 @@
+
+<?php
+$db_host = 'localhost';
+$db_user = 'root';
+$db_password = 'root';
+$db_db = 'td-web';
+$db_port = 8889;
+
+$mysqli = new mysqli(
+  $db_host,
+  $db_user,
+  $db_password,
+  $db_db
+);
+
+if ($mysqli->connect_error) {
+  echo 'Errno: ' . $mysqli->connect_errno;
+  echo '<br>';
+  echo 'Error: ' . $mysqli->connect_error;
+  exit();
+}
+
+echo 'Success: A proper connection to MySQL was made.';
+echo '<br>';
+echo 'Host information: ' . $mysqli->host_info;
+echo '<br>';
+echo 'Protocol version: ' . $mysqli->protocol_version;
+
+$mysqli->close();
+    //     $sql="SELECT titres FROM annonces";
