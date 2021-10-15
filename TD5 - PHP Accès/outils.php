c44bff0a29dd72b799007e0cbd9b739e3548da21
Mathieu Ranc <dev@mathieuranc.fr>
2021-10-15 13:56:07 +0200

    déconnection seulement quand on est connecté

--- TD5 - PHP Accès/outils.php
+++ TD5 - PHP Accès/outils.php
@@ -22,16 +22,18 @@ function afficheMenu()
 {
     echo '
 <div id="menu">
-    <img src="../assets/images/h99999.jpg" /><br />
-    <p>Pr&eacute;nom NOM</p>
-    <p>13 rue de toul<br />59000 LILLE<br />03.28.38.48.58<br />
-    <a href="mailto:xxxxxx@hei.yncrea.fr">xxx@hei.yncrea.fr</a>
-    </p>
-    <ul class="menuGauche">
-    <li><a href="consulter.php">Consulter</a></li>
-    <li><a href="ajouter.php">Ajouter</a></li>
-    <li><a href="supprimer.php">Supprimer</a></li>
-    </ul>
-    <a href="disconnect.php">Déconnection</a>
-</div>';
+<img src="../assets/images/h99999.jpg" /><br />
+<p>Pr&eacute;nom NOM</p>
+<p>13 rue de toul<br />59000 LILLE<br />03.28.38.48.58<br />
+<a href="mailto:xxxxxx@hei.yncrea.fr">xxx@hei.yncrea.fr</a>
+</p>
+<ul class="menuGauche">
+<li><a href="consulter.php">Consulter</a></li>
+<li><a href="ajouter.php">Ajouter</a></li>
+<li><a href="supprimer.php">Supprimer</a></li>
+</ul>';
+    if (isset($_SESSION["auth"]) && $_SESSION["auth"] != 0) {
+        echo '<a href="disconnect.php">Déconnection</a>';
+    }
+    echo '</div>';
 }
